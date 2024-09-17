642c761228e167e9f66bf10b425ac6c92ae80455
Rafeyboi2112 <rafe.lepherd@gmail.com>
2024-09-17 15:09:53 +0100

    Push1

---
+++ Assets/Destroyer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    //Much like Start() and Update() methods, OnTriggerEnter2D is a special Unity method that is called by Unity automatically at a specific point - in this case, when
+    //something enters the trigger attached to this GameObject
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the GameObject that has collided with our trigger is tagged with CleanUp
+        if (collision.gameObject.tag == "CleanUp")
+        {
+            //Then we use this method to destroy it
+            Destroy(collision.gameObject);
+        }
+    }
+}
